a57697ea5f98ed95a17b332537923b39260ad21b
sanjeev <therealsanjeev0@gmail.com>
2022-02-23 10:27:11 +0530

    get and post request

--- calc/views.py
+++ calc/views.py
@@ -7,3 +7,10 @@ def home(request):
     return render(request,'home.html',{
         'name':'Sanjeev!'
     })
+def add(request):
+    num1=request.GET['num1']
+    num2=request.GET['num2']
+    val=int(num1)+int(num2)
+    return render(request,'result.html',{
+        'result':val
+    })
